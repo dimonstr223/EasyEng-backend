c4ea6753d9445352757588d9c71c06b0c71d2575
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-24 09:22:35 +0300

    UserSchema created

---
+++ models/User.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+	username: { type: String, unique: true, required: true },
+	password: { type: String, required: true },
+	cards: { type: Array },
+	roles: { type: String, ref: 'Role' },
+})
+
+export default mongoose.model('User', UserSchema)
