81177b029d45bff8ecd88f631849bbfb4efe6338
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-24 06:04:18 +0300

    type: module and start:dev script added

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 const PORT = 6000
 
-app.listen(PORT, () => console.log(`Rerver is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
