ea0560d4768c84508f70c19282c84ad0940fe904
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-24 09:09:04 +0300

    AuthController class created

---
+++ controllers/AuthController.js
@@ -0,0 +1,16 @@
+class AuthController {
+	async register(req, res) {
+		try {
+		} catch (error) {}
+	}
+	async login(req, res) {
+		try {
+		} catch (error) {}
+	}
+	async getUsers(req, res) {
+		try {
+		} catch (error) {}
+	}
+}
+
+export default new AuthController()
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
+import AuthController from '../controllers/AuthController'
 
 const router = new Router()
 
-router.post('/register')
-router.post('/login')
-router.get('/users')
+router.post('/register', AuthController.register)
+router.post('/login', AuthController.login)
+router.get('/users', AuthController.getUsers)
 
 export default router
