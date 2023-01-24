74ecb1b0d9c20d52e0648ba4fdb2ce490cce0339
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-24 10:06:50 +0300

    cards of UserSchema refactored

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const UserSchema = new mongoose.Schema({
 	username: { type: String, unique: true, required: true },
 	password: { type: String, required: true },
-	cards: { type: Array, required: true, default: [] },
+	// cards: [{ type: Object, ref: 'Card', required: true }],
+	cards: [{ type: Object, required: true, ref: 'Card' }],
 	roles: [{ type: String, ref: 'Role' }],
 })
 
