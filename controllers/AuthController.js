7ea78e21ae61d2f40e02a94fb8d80468206f77b8
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-24 10:00:45 +0300

    register controller created, bcrypt installed

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,31 @@
+import Role from '../models/Role.js'
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+
 class AuthController {
 	async register(req, res) {
 		try {
-		} catch (error) {}
+			const { username, password } = req.body
+			const candidate = await User.findOne({ username })
+			if (candidate) {
+				return res.status(400).json({ message: 'This username already exists' })
+			}
+			const salt = bcrypt.genSaltSync(10)
+			const hashPassword = bcrypt.hashSync(password, salt)
+			const userRole = await Role.findOne({ value: 'USER' })
+			const cards = []
+			const user = new User({
+				username,
+				password: hashPassword,
+				roles: [userRole.value],
+				cards,
+			})
+			await user.save()
+			res.json({ message: 'Registrationg success!' })
+		} catch (error) {
+			console.log(error)
+			res.status(400).json({ message: 'Registration error' })
+		}
 	}
 	async login(req, res) {
 		try {
--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 const UserSchema = new mongoose.Schema({
 	username: { type: String, unique: true, required: true },
 	password: { type: String, required: true },
-	cards: { type: Array },
-	roles: { type: String, ref: 'Role' },
+	cards: { type: Array, required: true, default: [] },
+	roles: [{ type: String, ref: 'Role' }],
 })
 
 export default mongoose.model('User', UserSchema)
