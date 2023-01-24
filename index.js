422e66fd343b860f06e31b147f21bf5e58d13b0e
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-24 06:15:22 +0300

    cartRoutes created

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import express from 'express'
+import express, { Router } from 'express'
+import router from './routes/cartRoutes'
+
+const PORT = 5555
 
 const app = express()
 
-const PORT = 5555
+app.use('/api', router)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
