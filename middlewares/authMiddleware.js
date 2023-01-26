cf0631aed8ca54ab7d84a7c24ce86619aa8994e7
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-26 07:44:54 +0300

    authMiddleware created

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken'
+import config from '../config.js'
+
+export default (req, res, next) => {
+	if (req.method === 'OPTIONS') next()
+
+	try {
+		const token = req.headers.authorization.split(' ')[1]
+		if (!token) {
+			return res.status(403).json({ message: 'User not authorized' })
+		}
+		const decoded = jwt.verify(token, config.secret)
+		req.user = decoded
+		next()
+	} catch (error) {
+		console.log(error)
+		return res.status(403).json({ message: 'User not authorized' })
+	}
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
+
 import AuthController from '../controllers/AuthController.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
 import registerValidation from '../validations/registerValidation.js'
 
 const router = new Router()
 
 router.post('/register', registerValidation, AuthController.register)
 router.post('/login', AuthController.login)
-router.get('/users', AuthController.getUsers)
+router.get('/users', authMiddleware, AuthController.getUsers)
 
 export default router
