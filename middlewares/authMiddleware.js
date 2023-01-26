83706246876be9a7f8d1078bc6c413e476a69c3f
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-26 08:42:51 +0300

    authMiddleware token refactored

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ export default (req, res, next) => {
 	if (req.method === 'OPTIONS') next()
 
 	try {
-		const token = req.headers.authorization.split(' ')[1]
+		const token = (req.headers.authorization || '').split(' ')[1]
 		if (!token) {
 			return res.status(403).json({ message: 'User not authorized' })
 		}
