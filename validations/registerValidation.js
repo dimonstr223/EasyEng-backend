2531e9e581222be44deb8b9e74aca3667b12275a
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-27 09:10:38 +0300

    username min length changet to 3

--- validations/registerValidation.js
+++ validations/registerValidation.js
@@ -2,7 +2,7 @@ import { body } from 'express-validator'
 
 const registerValidation = [
 	body('username', 'Username must contain at least 4 symbols').isLength({
-		min: 4,
+		min: 3,
 	}),
 	body('password', 'Password must contain 4-10 symbols').isLength({
 		min: 4,
