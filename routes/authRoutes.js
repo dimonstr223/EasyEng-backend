484761ef4a106de24081b0f5eabe0b1cedfdd6fc
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-28 09:00:56 +0300

    register route changed to signup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,11 @@ import { Router } from 'express'
 
 import AuthController from '../controllers/AuthController.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
-import registerValidation from '../validations/registerValidation.js'
+import signUpValidation from '../validations/signUpValidation.js'
 
 const router = new Router()
 
-router.post('/register', registerValidation, AuthController.register)
+router.post('/signup', signUpValidation, AuthController.register)
 router.post('/login', AuthController.login)
 router.get('/users', authMiddleware, AuthController.getUsers)
 router.get('/me', AuthController.getMe)
