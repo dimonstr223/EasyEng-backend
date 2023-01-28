29b4f325f8851399d6acba8bc471e599e340ca77
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-28 10:37:04 +0300

    login controller refactored

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,14 +7,6 @@ import User from '../models/User.js'
 
 dotenv.config()
 
-const generateAccessToken = (id, roles) => {
-	const payload = {
-		id,
-		roles,
-	}
-	return jwt.sign(payload, config.secret, { expiresIn: '24h' })
-}
-
 class AuthController {
 	async signUp(req, res) {
 		try {
@@ -50,7 +42,7 @@ class AuthController {
 			})
 			const user = await doc.save()
 
-			// Create JWT
+			// Sign and send JWT
 			const accessToken = jwt.sign(
 				{ _id: user._id },
 				process.env.ACCESS_TOKEN_SECRET,
@@ -65,19 +57,33 @@ class AuthController {
 	async login(req, res) {
 		try {
 			const { username, password } = req.body
+
 			const user = await User.findOne({ username })
+
+			// If the user not found
 			if (!user) {
-				return res.status(400).json({ message: 'Invalid login or password' })
+				return res.status(400).json({ message: 'Invalid credentials' })
 			}
-			const isValidPassword = bcrypt.compareSync(password, user.password)
-			if (!isValidPassword) {
-				return res.status(400).json({ message: 'Invalid login or password' })
+
+			// Compare hachPassword with user password
+			const isMatch = await bcrypt.compare(password, user.password)
+
+			// If passwords not matched
+			if (!isMatch) {
+				return res.status(400).json({ message: 'Invalid credentials' })
 			}
-			const token = generateAccessToken(user._id, user.roles)
-			res.json({ token })
+
+			// Sign and send JWT
+			const accessToken = await jwt.sign(
+				{ _id: user._id },
+				process.env.ACCESS_TOKEN_SECRET,
+				{ expiresIn: '10m' }
+			)
+
+			res.json({ accessToken })
 		} catch (error) {
 			console.log(error)
-			res.status(400).json({ message: 'Authorization error' })
+			res.status(401).json({ message: 'Authorization error' })
 		}
 	}
 	async getUsers(req, res) {
