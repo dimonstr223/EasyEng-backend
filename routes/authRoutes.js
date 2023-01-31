2ad4a9ae5677565abc597528f1084328e23f0fcb
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-31 11:30:18 +0300

    add upload middleware to signup route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,11 +4,17 @@ import jwt from 'jsonwebtoken'
 import AuthController from '../controllers/AuthController.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
 import Token from '../models/Token.js'
+import upload from '../multer/multer.js'
 import signUpValidation from '../validations/signUpValidation.js'
 
 const router = new Router()
 
-router.post('/signup', signUpValidation, AuthController.signUp)
+router.post(
+	'/signup',
+	signUpValidation,
+	upload.single('avatar'),
+	AuthController.signUp
+)
 router.post('/login', AuthController.login)
 router.delete('/logout', AuthController.logOut)
 // Create new access token from refresh token
