adfa6fb8f6c37d86bc5aeac5b0cc557f0ae4f20b
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-31 14:54:31 +0300

    route to update user created

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,6 +11,7 @@ const router = new Router()
 router.post('/signup', signUpValidation, AuthController.signUp)
 router.post('/upload', upload.single('avatar'), AuthController.uploadAvatar)
 router.post('/login', AuthController.login)
+router.patch('/users/:id', AuthController.updateUser)
 router.delete('/logout', AuthController.logOut)
 // Create new access token from refresh token
 router.post('/token', AuthController.generateAccessToken)
