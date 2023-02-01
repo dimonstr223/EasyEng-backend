58499ea3e595f9158879c5a05658261b640be67f
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-01 06:09:25 +0300

    authMiddleware added to updateUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ import authMiddleware from '../middlewares/authMiddleware.js'
 const router = new Router()
 
 router.get('/users', authMiddleware, UserController.getUsers)
-router.patch('/users/:id', UserController.updateUser)
+router.patch('/users/:id', authMiddleware, UserController.updateUser)
 
 export default router
