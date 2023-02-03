364b7ca838040b4a28576d5a2b0f7910ed7b7c8c
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-03 11:52:32 +0300

    paginationrefactoring

--- utils/pagination.js
+++ utils/pagination.js
@@ -2,20 +2,7 @@ const pagination = (items, page, limit) => {
 	const startIndex = (page - 1) * limit
 	const endIndex = page * limit
 
-	const result = { totalCount: items.length }
-
-	if (startIndex > 0) {
-		result.prev = {
-			page: page - 1,
-			limit,
-		}
-	}
-	if (endIndex < items.length) {
-		result.next = {
-			page: page + 1,
-			limit,
-		}
-	}
+	const result = { totalCount: items.length, currentPage: page, limit }
 
 	result.items = items.reverse().slice(startIndex, endIndex)
 	return result
