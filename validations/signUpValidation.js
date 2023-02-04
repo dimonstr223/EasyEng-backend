7d52ef881ef212f3164ee42a30731bc84f97bd04
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-04 18:29:43 +0300

    max username length in signUpCalidation added

--- validations/signUpValidation.js
+++ validations/signUpValidation.js
@@ -3,6 +3,7 @@ import { check } from 'express-validator'
 const signUpValidation = [
 	check('username', 'Invalid username').isLength({
 		min: 3,
+		max: 15,
 	}),
 	check('password', 'Password must be 4-15 characters').isLength({
 		min: 4,
