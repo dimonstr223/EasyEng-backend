e15407b1795e03a035b150b1710be9a7a48b7073
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-05 13:43:40 +0300

    recreate CardModel

--- models/card.js
+++ models/card.js
@@ -1,14 +1,10 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose from 'mongoose'
 
 const CardSchema = new mongoose.Schema({
 	word: { type: String, required: true },
 	translation: { type: String, required: true },
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	imageURL: { type: String },
+	imageURL: String,
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 })
 
 export default mongoose.model('Card', CardSchema)
