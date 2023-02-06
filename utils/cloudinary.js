8b6153460b9084f6898c27d20bf63bd76fae2c03
dimonstr223 <dimstukmikolu@gmail.com>
2023-02-06 08:35:26 +0300

    .env to cloudinary

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,12 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary'
 
 cloudinary.config({
-	cloud_name: 'dxdjl7htw',
-	api_key: '938738556183128',
-	api_secret: 'D7p71cxdbtDxIQWHBH9UXgCyHjM',
-	// cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-	// api_key: process.env.CLOUDINARY_CLIENT_API,
-	// api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+	api_key: process.env.CLOUDINARY_CLIENT_API,
+	api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
 })
 
 const opts = {
